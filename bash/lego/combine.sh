88b59e0057ee070db1c0fd74f8f28ac7dddab673
Gorka Prieto <gorka.prieto@ehu.eus>
2016-09-23 09:53:23 +0200

    Included protein-level competition

--- bash/lego/combine.sh
+++ bash/lego/combine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $# -ne 5 ]; then
-	echo -e "Usage:\n\t$0 <inputDir1> <prefixFile1> <inputDir2> <prefixFile2> <outputDir>"
+if [ $# -ne 6 ]; then
+	echo -e "Usage:\n\t$0 <inputDir1> <prefixFile1> <inputDir2> <prefixFile2> <outputDir> <prefixComb>"
 	exit
 fi
 
@@ -10,6 +10,7 @@ PREFIX1="$2"
 INPUT2="$3"
 PREFIX2="$4"
 OUTPUT="$5"
+OPREFIX="$6"
 
 # =======
 # Runtime
@@ -19,7 +20,7 @@ LEGO=/home/gorka/Bio/Proyectos/Proteómica/spHPP/Work/Flow/lego
 JAR="$LEGO/EhuBio.jar"
 LOGGING="$LEGO/logging.properties"
 OPTS="-Xmx10g -Djava.util.logging.config.file=$LOGGING -Djava.awt.headless=true"
-USE_MAYU=true
+FDR=MAYU
 
 module() {
     java -cp "$JAR" $OPTS org.sphpp.workflow.module.$@ 2>&1 | tee -a "$OUTPUT/log.txt"
@@ -32,13 +33,13 @@ module() {
 
 mkdir -p "$OUTPUT"
 
-TARGET="$OUTPUT/CombTarget.tsv.gz"
-DECOY="$OUTPUT/CombDecoy.tsv.gz"
+TARGET="$OUTPUT/${OPREFIX}Target.tsv.gz"
+DECOY="$OUTPUT/${OPREFIX}Decoy.tsv.gz"
 
-FDRTARGET="$OUTPUT/FdrCombTarget.tsv.gz"
-FDRDECOY="$OUTPUT/FdrCombDecoy.tsv.gz"
+FDRTARGET="$OUTPUT/Fdr${OPREFIX}Target.tsv.gz"
+FDRDECOY="$OUTPUT/Fdr${OPREFIX}Decoy.tsv.gz"
 
 module Combinator --input1 "$INPUT1/${PREFIX1}Target.tsv.gz" --input2 "$INPUT2/${PREFIX2}Target.tsv.gz" --output "$TARGET"
 module Combinator --input1 "$INPUT1/${PREFIX1}Decoy.tsv.gz" --input2 "$INPUT2/${PREFIX2}Decoy.tsv.gz" --output "$DECOY"
 
-module FdrCalculator --inTarget "$TARGET" --inDecoy "$DECOY" --outTarget "$FDRTARGET" --outDecoy "$FDRDECOY" --useMayu "$USE_MAYU"
+module FdrCalculator --inTarget "$TARGET" --inDecoy "$DECOY" --outTarget "$FDRTARGET" --outDecoy "$FDRDECOY" --type "$FDR"
