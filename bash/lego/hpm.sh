26b18faf4915818ed2ddaff98cf74ef64c8f2c8b
Gorka Prieto <gorka.prieto@ehu.eus>
2018-12-17 18:47:28 +0100

    Updated hpm script

--- bash/lego/hpm.sh
+++ bash/lego/hpm.sh
@@ -4,25 +4,28 @@ export TARGET_FASTA=/media/gorka/EhuBio/Fasta/gencode25.target.IL.gorka.fasta
 export DECOY_FASTA=/media/gorka/EhuBio/Fasta/gencode25.decoy.IL.gorka.fasta
 INPUT_DIR=/media/gorka/EhuBio/Search
 OUTPUT_DIR=/media/gorka/EhuBio/Lego
-ENGINES="XTandem"
+
+#ENGINES="XTandem"
 #ENGINES="XTandem Comet Fragger"
 #ENGINES="Sequest XTandem-MGF XTandem-mzML Comet"
 #ENGINES="XTandem+Comet-NoCal XTandem+Comet-Cal"
 #ENGINES="Comet"
-#ENGINES="Comet2"
+ENGINES="Comet2"
 #ENGINES="Comet3"
 #ENGINES="Comet5"
 #ENGINES="Fragger"
 #ENGINES="XTandem+Fragger-NoCal XTandem+Fragger-Cal"
+
 #SCORES="LPF LPM LPG LPG1 LPGN LPGB"
-SCORES="LP"
+SCORES="LPM LPF LPGN"
+
 #FDRS="NORMAL MAYU PICKED REFINED"
-FDRS="NORMAL"
+FDRS="NORMAL PICKED REFINED"
+
+TISSUES="Adult_Liver Adult_Testis"
 
 #for T in "$INPUT_DIR"/Adult* "$INPUT_DIR"/Fetal*; do
-#for T in All_Tissues; do
-#for T in Adult_Heart; do
-for T in Adult_Liver; do
+for T in $TISSUES; do
 	T=`basename "$T"`
 	for E in $ENGINES; do
 		for S in $SCORES; do
